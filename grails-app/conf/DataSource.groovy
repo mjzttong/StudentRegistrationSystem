f58a54e16586a62e19bdce95c83cd5bffc14b391
Maynard Tong <maynardtong@gmail.com>
2012-05-04 02:23:28 +0800

    Reverted Datasource back to mysql from in memory
    Signed-off-by: Maynard Tong <maynardtong@gmail.com>

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -14,7 +14,7 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-        url = "jdbc:h2:mem:testDb;MVCC=TRUE"
+        url = "jdbc:mysql://localhost/schoolregistration"
         }
     }
     test {
